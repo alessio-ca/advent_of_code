4adb3fd78abfbdf5af0e3bb5b262615d2ace8433
Alessio <alessio.caciagli@okra.ai>
2021-02-13 19:33:20 +0100

    add solution for challenge 4

--- utils.py
+++ utils.py
@@ -8,3 +8,18 @@ def read_input(input_file: str) -> List[str]:
             input_list.append(line.strip())
 
     return input_list
+
+
+def read_input_batch(input_file: str) -> List[str]:
+    with open(input_file, "r") as input_file:
+        input_list = []
+        batch = []
+        for line in input_file:
+            if line == "\n":
+                input_list.append(batch)
+                batch = []
+            else:
+                for element in line.strip().split(" "):
+                    batch.append(element)
+
+    return input_list

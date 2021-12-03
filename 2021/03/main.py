1467456e32482ff68c68b864565fe5856465b28c
Alessio <alessio.caciagli@okra.ai>
2021-12-03 08:31:44 +0000

    formatting

--- 2021/03/main.py
+++ 2021/03/main.py
@@ -34,8 +34,8 @@ Considering only the first bit of each number, there are five 0 bits and seven 1
 The most common second bit of the numbers in the diagnostic report is 0, so the second
  bit of the gamma rate is 0.
 
-The most common value of the third, fourth, and fifth bits are 1, 1, and 0, respectively,
- and so the final three bits of the gamma rate are 110.
+The most common value of the third, fourth, and fifth bits are 1, 1, and 0,
+ respectively, and so the final three bits of the gamma rate are 110.
 
 So, the gamma rate is the binary number 10110, or 22 in decimal.
 

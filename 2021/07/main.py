60bbc70f2e8ef3b33b5337fd95b82b109bbfe13b
Alessio <alessio.caciagli@okra.ai>
2021-12-07 18:00:19 +0000

    refined bounds for line search

--- 2021/07/main.py
+++ 2021/07/main.py
@@ -83,8 +83,13 @@ def minimize_fuel(X: np.ndarray, constant_rate: bool) -> float:
     """Simple function to optimize fuel consumption"""
     # Define initial guess for min_fuel (overestimate)
     min_fuel = X.sum() if constant_rate else int((X * (X + 1) * 0.5).sum())
+
     # Define limits for the line search
-    min_pos, max_pos = X.min(), X.max()
+    # One can demonstrate that the optimal fuel value is bounded by the mean of X via:
+    # X_mean - 0.5 <= i <= X_mean + 0.5
+    X_mean = X.mean()
+    min_pos, max_pos = int(np.floor(X_mean - 0.5)), int(np.ceil(X_mean + 0.5))
+
     # Perform line search
     for i in range(min_pos, max_pos):
         # Define the absolute step number between candidate position and any crab

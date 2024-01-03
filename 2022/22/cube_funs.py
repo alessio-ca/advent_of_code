5db8af2117ef9edf4c6c0a2bf6266e66af25ea55
Alessio <alessio.caciagli@okra.ai>
2024-01-03 13:45:17 +0100

    committed solution to problem 22 2022 (finally)

---
+++ 2022/22/cube_funs.py
@@ -0,0 +1,148 @@
+from .main import CubeTuple, Node, CoordTuple
+from typing import Dict, Deque
+
+
+def remap_edges_example(dict_nodes: Dict[CubeTuple, Node], size: int):
+    max_idx = size - 1
+    for i in range(size):
+        # 0 --> 1
+        setattr(dict_nodes[(0, i, 0)], "up", (0, max_idx - i, 1))
+        setattr(dict_nodes[(0, max_idx - i, 1)], "up", (0, i, 0))
+        # 0 --> 2
+        setattr(dict_nodes[(i, 0, 0)], "left", (0, i, 2))
+        setattr(dict_nodes[(0, i, 2)], "up", (i, 0, 0))
+        # 0 --> 3
+        setattr(dict_nodes[(max_idx, i, 0)], "down", (0, i, 3))
+        setattr(dict_nodes[(0, i, 3)], "up", (max_idx, i, 0))
+        # 0 --> 5
+        setattr(dict_nodes[(i, max_idx, 0)], "right", (max_idx - i, 0, 5))
+        setattr(dict_nodes[(max_idx - i, 0, 5)], "right", (i, max_idx, 0))
+        # 1 --> 2
+        setattr(dict_nodes[(i, max_idx, 1)], "right", (i, 0, 2))
+        setattr(dict_nodes[(i, 0, 2)], "left", (i, max_idx, 1))
+        # 1 --> 4
+        setattr(dict_nodes[(max_idx, i, 1)], "down", (max_idx, max_idx - i, 4))
+        setattr(dict_nodes[(max_idx, max_idx - i, 4)], "down", (max_idx, i, 1))
+        # 1 --> 5
+        setattr(dict_nodes[(i, 0, 1)], "left", (max_idx, max_idx - i, 5))
+        setattr(dict_nodes[(max_idx, max_idx - i, 5)], "down", (i, 0, 1))
+        # 2 --> 3
+        setattr(dict_nodes[(i, max_idx, 2)], "right", (i, 0, 3))
+        setattr(dict_nodes[(i, 0, 3)], "left", (i, max_idx, 2))
+        # 2 --> 4
+        setattr(dict_nodes[(max_idx, i, 2)], "down", (max_idx - i, 0, 4))
+        setattr(dict_nodes[(max_idx - i, 0, 4)], "left", (max_idx, i, 2))
+        # 3 --> 4
+        setattr(dict_nodes[(max_idx, i, 3)], "down", (0, i, 4))
+        setattr(dict_nodes[(0, i, 4)], "up", (max_idx, i, 3))
+        # 3 --> 5
+        setattr(dict_nodes[(i, max_idx, 3)], "right", (0, max_idx - i, 5))
+        setattr(dict_nodes[(0, max_idx - i, 5)], "top", (i, max_idx, 3))
+        # 4 --> 5
+        setattr(dict_nodes[(i, max_idx, 4)], "right", (i, 0, 5))
+        setattr(dict_nodes[(i, 0, 5)], "left", (i, max_idx, 4))
+
+
+def turn_direction_example(
+    pos: CubeTuple,
+    new_pos: CubeTuple,
+    directions: Deque[CoordTuple],
+):
+    # Change direction if moving to another face of the cube
+    id_1, id_2 = pos[2], new_pos[2]
+    if id_1 != id_2:
+        if (
+            (id_1, id_2) == (0, 1)
+            or (id_1, id_2) == (1, 0)
+            or (id_1, id_2) == (0, 5)
+            or (id_1, id_2) == (5, 0)
+            or (id_1, id_2) == (1, 4)
+            or (id_1, id_2) == (4, 1)
+        ):
+            directions.rotate(2)
+        elif (
+            (id_1, id_2) == (0, 2)
+            or (id_1, id_2) == (2, 4)
+            or (id_1, id_2) == (5, 1)
+            or (id_1, id_2) == (5, 3)
+        ):
+            directions.rotate(1)
+        elif (
+            (id_1, id_2) == (2, 0)
+            or (id_1, id_2) == (4, 2)
+            or (id_1, id_2) == (1, 5)
+            or (id_1, id_2) == (3, 5)
+        ):
+            directions.rotate(-1)
+
+
+def remap_edges_input(dict_nodes: Dict[CubeTuple, Node], size: int):
+    max_idx = size - 1
+    for i in range(size):
+        # 0 --> 1
+        setattr(dict_nodes[(i, max_idx, 0)], "right", (i, 0, 1))
+        setattr(dict_nodes[(i, 0, 1)], "left", (i, max_idx, 0))
+        # 0 --> 2
+        setattr(dict_nodes[(max_idx, i, 0)], "down", (0, i, 2))
+        setattr(dict_nodes[(0, i, 2)], "up", (max_idx, i, 0))
+        # 0 --> 3
+        setattr(dict_nodes[(i, 0, 0)], "left", (max_idx - i, 0, 3))
+        setattr(dict_nodes[(max_idx - i, 0, 3)], "left", (i, 0, 0))
+        # 0 --> 5
+        setattr(dict_nodes[(0, i, 0)], "up", (i, 0, 5))
+        setattr(dict_nodes[(i, 0, 5)], "left", (0, i, 0))
+        # 1 --> 2
+        setattr(dict_nodes[(max_idx, i, 1)], "down", (i, max_idx, 2))
+        setattr(dict_nodes[(i, max_idx, 2)], "right", (max_idx, i, 1))
+        # 1 --> 4
+        setattr(dict_nodes[(i, max_idx, 1)], "right", (max_idx - i, max_idx, 4))
+        setattr(dict_nodes[(max_idx - i, max_idx, 4)], "right", (i, max_idx, 1))
+        # 1 --> 5
+        setattr(dict_nodes[(0, i, 1)], "up", (max_idx, i, 5))
+        setattr(dict_nodes[(max_idx, i, 5)], "down", (0, i, 1))
+        # 2 --> 3
+        setattr(dict_nodes[(i, 0, 2)], "left", (0, i, 3))
+        setattr(dict_nodes[(0, i, 3)], "up", (i, 0, 2))
+        # 2 --> 4
+        setattr(dict_nodes[(max_idx, i, 2)], "down", (0, i, 4))
+        setattr(dict_nodes[(0, i, 4)], "up", (max_idx, i, 2))
+        # 3 --> 4
+        setattr(dict_nodes[(i, max_idx, 3)], "right", (i, 0, 4))
+        setattr(dict_nodes[(i, 0, 4)], "left", (i, max_idx, 3))
+        # 3 --> 5
+        setattr(dict_nodes[(max_idx, i, 3)], "down", (0, i, 5))
+        setattr(dict_nodes[(0, i, 5)], "up", (max_idx, i, 3))
+        # 4 --> 5
+        setattr(dict_nodes[(max_idx, i, 4)], "down", (i, max_idx, 5))
+        setattr(dict_nodes[(i, max_idx, 5)], "right", (max_idx, i, 4))
+
+
+def turn_direction_input(
+    pos: CubeTuple,
+    new_pos: CubeTuple,
+    directions: Deque[CoordTuple],
+):
+    # Change direction if moving to another face of the cube
+    id_1, id_2 = pos[2], new_pos[2]
+    if id_1 != id_2:
+        if (
+            (id_1, id_2) == (0, 3)
+            or (id_1, id_2) == (3, 0)
+            or (id_1, id_2) == (1, 4)
+            or (id_1, id_2) == (4, 1)
+        ):
+            directions.rotate(2)
+        elif (
+            (id_1, id_2) == (2, 3)
+            or (id_1, id_2) == (5, 0)
+            or (id_1, id_2) == (2, 1)
+            or (id_1, id_2) == (5, 4)
+        ):
+            directions.rotate(1)
+        elif (
+            (id_1, id_2) == (3, 2)
+            or (id_1, id_2) == (0, 5)
+            or (id_1, id_2) == (1, 2)
+            or (id_1, id_2) == (4, 5)
+        ):
+            directions.rotate(-1)
--- 2022/22/main.py
+++ 2022/22/main.py
@@ -2,8 +2,16 @@ from utils import read_input
 import math
 import re
 import itertools
+from collections import deque
+from typing import Union, List, Dict, Tuple, TypeVar, Callable
+from . import cube_funs as cb
 
-DIR_MAP = {"right": 0, "down": 1, "left": 2, "up": 3}
+T = TypeVar("T", bound=int)
+CoordTuple = Tuple[T, T]
+CubeTuple = Tuple[T, T, T]
+
+DIR_MAP = {(0, 1): "right", (1, 0): "down", (0, -1): "left", (-1, 0): "up"}
+FACING_MAP = {(0, 1): 0, (1, 0): 1, (0, -1): 2, (-1, 0): 3}
 
 
 def find_match(c, line):
@@ -13,51 +21,62 @@ def find_match(c, line):
         return math.inf
 
 
+def generate_instructions(input_file: List[str]) -> List[Union[str, int]]:
+    steps = list(map(int, re.findall(r"\d+", input_file[-1].strip())))
+    rot = re.findall(r"[A-Z]", input_file[-1].strip())
+    return [
+        x for x in itertools.chain(*itertools.zip_longest(steps, rot)) if x is not None
+    ]
+
+
 class Node:
-    def __init__(self, c, v, id):
+    def __init__(self, c, v, id=-1):
         self.x, self.y = c
         self.v = v
         self.id = id
-
-        # Initialise neighs
         self.up = None
-        self.up_flip = None
         self.down = None
-        self.down_flip = None
-        self.right = None
-        self.right_flip = None
         self.left = None
-        self.left_flip = None
+        self.right = None
 
 
-class LinkedNode:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        self.prev = None
-
-
-class CircularDoublyLinkedList:
-    def __init__(self):
-        self.head = None
-
-    def insert_after(self, ref_node, new_node):
-        new_node.prev = ref_node
-        new_node.next = ref_node.next
-        new_node.next.prev = new_node
-        ref_node.next = new_node
-
-    def insert_at_end(self, new_node):
-        if self.head is None:
-            self.head = new_node
-            new_node.next = new_node
-            new_node.prev = new_node
-        else:
-            self.insert_after(self.head.prev, new_node)
-
-
-def create_node_system(input_file, size):
+def create_node_system(input_file: List[str]) -> Dict[CubeTuple, Node]:
+    # Create flat node system
     dict_nodes = {}
+    for i, line in enumerate(input_file[:-2]):
+        offset = min(find_match(".", line), find_match("#", line))
+        line = line.strip()
+        # For flat map, face_id is not important - set it to -1
+        for j, v in enumerate(line):
+            coords = (i, j + offset)
+            dict_nodes[(i, j + offset, -1)] = Node(coords, v)
+    # Populate nn for each node
+    # idx corresponds to the axis:
+    # 0 sweeps the y axis
+    # 1 sweeps the x axis
+    for idx in [0, 1]:
+        gen = [coord[idx] for coord in dict_nodes.keys()]
+        min_c, max_c = min(gen), max(gen)
+        # Generate sweep line
+        for i in range(min_c, max_c + 1):
+            array = [key for key in dict_nodes.keys() if key[idx] == i]
+            array.sort(key=lambda x: x[0])
+            for low, high in zip(array, array[1:] + array[:1]):
+                # When sweeping on the y axis, set right and left
+                if idx == 0:
+                    setattr(dict_nodes[low], "right", high)
+                    setattr(dict_nodes[high], "left", low)
+                # When sweeping on the x axis, set down and up
+                else:
+                    setattr(dict_nodes[low], "down", high)
+                    setattr(dict_nodes[high], "up", low)
+
+    return dict_nodes
+
+
+def initialise_cube_coords(input_file: List[str], size: int) -> List[Node]:
+    # For the cube case, add the face_id when parsing the input
+    nodes = []
     idx = -1
     counter = 0
     for i, line in enumerate(input_file[:-2]):
@@ -66,70 +85,116 @@ def create_node_system(input_file, size):
 
         chunks = [line[x : x + size] for x in range(0, len(line), size)]
         for block, chunk in enumerate(chunks):
-            for j, c in enumerate(chunk):
-                coords = (block * size + j + offset, i)
-                dict_nodes[coords] = Node(coords, c, idx + 1 + block)
+            for j, v in enumerate(chunk):
+                coords = (i, block * size + j + offset)
+                nodes.append(Node(coords, v, idx + 1 + block))
 
         counter += 1
         if (counter % size) == 0:
             counter = 0
             idx += 1 * (len(line) // size)
-    # Populate directions
-    for idx in [0, 1]:
-        gen = [coord[idx] for coord in dict_nodes.keys()]
-        min_c, max_c = min(gen), max(gen)
-        for i in range(min_c, max_c + 1):
-            array = [key for key in dict_nodes.keys() if key[idx] == i]
-            array.sort(key=lambda x: x[0])
-            for low, high in zip(array, array[1:] + array[:1]):
-                if idx == 0:
-                    setattr(dict_nodes[low], "down", high)
-                    setattr(dict_nodes[high], "up", low)
-                else:
-                    setattr(dict_nodes[low], "right", high)
-                    setattr(dict_nodes[high], "left", low)
-    return dict_nodes
+
+    return nodes
 
 
-def walk_maze(dict_nodes, instructions, dirlist):
-    pos = (min(x for x, y in dict_nodes.keys() if y == 0), 0)
+def create_cube_system(
+    nodes: List[Node],
+    size: int,
+    remap_edges_fun: Callable,
+) -> Tuple[Dict[CubeTuple, Node], Dict[CubeTuple, CubeTuple]]:
+    dict_nodes = {}
+    remap_nodes = {}
+
+    max_id = max(node.id for node in nodes) + 1
+    # Normalise coords to the (0, size) range for each face
+    for face_id in range(max_id):
+        face_nodes = [node for node in nodes if node.id == face_id]
+        min_x, min_y = min(node.x for node in face_nodes), min(
+            node.y for node in face_nodes
+        )
+        # Before normalising, store the original position
+        # (we need it for the final result)
+        for node in face_nodes:
+            remap_nodes[(node.x - min_x, node.y - min_y, face_id)] = (
+                node.x,
+                node.y,
+                face_id,
+            )
+            node.x -= min_x
+            node.y -= min_y
+    # Initialise nodes, ignore edges for now
+    for node in nodes:
+        key = (node.x, node.y, node.id)
+        dict_nodes[key] = node
+        setattr(dict_nodes[key], "down", (node.x + 1, node.y, node.id))
+        setattr(dict_nodes[key], "up", (node.x - 1, node.y, node.id))
+        setattr(dict_nodes[key], "right", (node.x, node.y + 1, node.id))
+        setattr(dict_nodes[key], "left", (node.x, node.y - 1, node.id))
+
+    # Remap edges accordingly
+    remap_edges_fun(dict_nodes, size)
+
+    return dict_nodes, remap_nodes
+
+
+def walk_cube(
+    start_pos: CubeTuple,
+    dict_cube: Dict[CubeTuple, Node],
+    instructions: List[Union[int, str]],
+    turn_direction_fun: Callable,
+) -> Tuple[int, int]:
+    pos = start_pos
+    # Initialise direction as a deque, which can rotate
+    # to deal with direction changes
+    directions = deque([(0, 1), (1, 0), (0, -1), (-1, 0)])
     for move in instructions:
         if isinstance(move, int):
             # Attempt move
             for _ in range(move):
-                new_pos = getattr(dict_nodes[pos], dirlist.head.value)
-                if dict_nodes[new_pos].v == ".":
+                new_pos = getattr(dict_cube[pos], DIR_MAP[directions[0]])
+                if dict_cube[new_pos].v == ".":
+                    # Deal with direction changes if needed
+                    turn_direction_fun(pos, new_pos, directions)
                     pos = new_pos
                 else:
                     break
-
         else:
             # Rotate
             if move == "R":
-                dirlist.head = dirlist.head.next
+                directions.rotate(-1)
             else:
-                dirlist.head = dirlist.head.prev
-    return pos
+                directions.rotate(1)
+    return pos, directions[0]
 
 
 def main(filename: str):
     input_file = read_input(filename, line_strip=False)
-    dict_nodes = create_node_system(input_file, 4)
+    turn_direction_fun = (
+        cb.turn_direction_example if "example" in filename else cb.turn_direction_input
+    )
+    remap_edges_fun = (
+        cb.remap_edges_example if "example" in filename else cb.remap_edges_input
+    )
+    size_cube = 4 if "example" in filename else 50
 
-    llist = CircularDoublyLinkedList()
-    for val in DIR_MAP.keys():
-        llist.insert_at_end(LinkedNode(val))
-
-    steps = list(map(int, re.findall(r"\d+", input_file[-1].strip())))
-    rot = re.findall(r"[A-Z]", input_file[-1].strip())
-
-    instructions = [
-        x for x in itertools.chain(*itertools.zip_longest(steps, rot)) if x is not None
-    ]
-
-    pos = walk_maze(dict_nodes, instructions, llist)
-    res = 1000 * (pos[1] + 1) + 4 * (pos[0] + 1) + DIR_MAP[llist.head.value]
+    dict_nodes = create_node_system(input_file)
+    start_pos = (min(x for x, y, _ in dict_nodes.keys() if y == 0), 0, -1)
+    pos, facing = walk_cube(
+        start_pos, dict_nodes, generate_instructions(input_file), turn_direction_fun
+    )
+    res = 1000 * (pos[0] + 1) + 4 * (pos[1] + 1) + FACING_MAP[facing]
     print(f"Result of part 1: {res}")
+
+    dict_cube, remap_cube = create_cube_system(
+        initialise_cube_coords(input_file, size_cube), size_cube, remap_edges_fun
+    )
+    start_pos = (0, 0, 0)
+    pos, facing = walk_cube(
+        start_pos, dict_cube, generate_instructions(input_file), turn_direction_fun
+    )
+    pos = remap_cube[pos]
+    res = 1000 * (pos[0] + 1) + 4 * (pos[1] + 1) + FACING_MAP[facing]
+    print(f"Result of part 2: {res}")
 
 
 if __name__ == "__main__":
